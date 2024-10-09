2694ba5add93bc1df5ab536f3da5f119b8250173
cherifa.informatique@gmail.com <cherifa.informatique@gmail.com>
2024-10-09 10:17:19 +0100

    updated

--- src/main/java/bandeau/BandeauVerrouillable.java
+++ src/main/java/bandeau/BandeauVerrouillable.java
@@ -1,8 +1,17 @@
 package bandeau;
 
-public class BandeauVerrouillable extends Bandeau implements Runnable {
-    @Override
-    public void run() {
-        
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BandeauVerrouillable extends Bandeau {
+    // private final Lock lock = new ReentrantLock();
+    private final Lock verrou = new ReentrantLock();
+
+    public void verrouille() {
+        verrou.lock();
+    }
+
+    public void deverrouille() {
+        verrou.unlock();
     }
 }
